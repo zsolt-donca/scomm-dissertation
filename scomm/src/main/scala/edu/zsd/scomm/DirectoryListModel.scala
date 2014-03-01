5bde6585bb415e546f39ae542193d32a59f84e30
zsolt <zsolt@647c8dbb-8ae3-7f4d-84cd-31f5644c49d4>
2014-03-01 10:39:49 +0000

    Some minor touches.
    git-svn-id: https://zsolt-pc:12443/svn/projects-n/trunk@11 647c8dbb-8ae3-7f4d-84cd-31f5644c49d4

--- scomm/src/main/scala/edu/zsd/scomm/DirectoryListModel.scala
+++ scomm/src/main/scala/edu/zsd/scomm/DirectoryListModel.scala
@@ -1,20 +1,19 @@
 package edu.zsd.scomm
 
 import java.io.File
-import scala.react.{CachedSignal, Observing, Var}
+import scala.react.{Signal, CachedSignal, Observing, Var}
 
 class DirectoryListModel() extends Observing {
   val currentDir: Var[File] = new Var[File](new File("")) {
     override def toString: String = "DirectoryListModel.currentDir"
   }
 
-  val currentDirContents = new CachedSignal({
+  val currentDirContents: Signal[Seq[FileEntry]] = new CachedSignal({
     val currentDir: File = DirectoryListModel.this.currentDir()
     println("DirectoryListModel.currentDirContents evaluated with: " + currentDir)
-    val listFiles: Seq[File] = currentDir.listFiles()
-      val contents: Seq[File] = if (listFiles != null) listFiles else List.empty
-      val parentFile: File = if (currentDir.getParentFile != null) currentDir.getParentFile else currentDir
-    val result: Seq[FileEntry] = List(FileEntry(parentFile, "..")) ++ contents.map(file => FileEntry(file, file.getName))
-    result
+    val listFiles = currentDir.listFiles()
+    val contents: Seq[FileEntry] = if (listFiles != null) listFiles.map(file => FileEntry(file, file.getName)) else Seq.empty
+    val parentFile: Seq[FileEntry] = if (currentDir.getParentFile != null) Seq(FileEntry(currentDir.getParentFile, "..")) else Seq.empty
+    parentFile ++ contents
   })
 }
