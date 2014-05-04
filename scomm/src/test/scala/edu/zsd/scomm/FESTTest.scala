175e84fae7df2e63eca2d1f60367755aa50f29a8
Zsolt Donca <zsolt.donca@hp.com>
2014-05-04 16:59:45 +0300

    Committing all stuff. Why the line separator change?

--- scomm/src/test/scala/edu/zsd/scomm/FESTTest.scala
+++ scomm/src/test/scala/edu/zsd/scomm/FESTTest.scala
@@ -9,6 +9,7 @@ import edu.zsd.scomm.useractions.{MainWindowUserActions, DirectoryListUserAction
 object FESTTest {
 
   val testDir : String = new File(this.getClass.getClassLoader.getResource("testDir").toURI).getPath
+  deleteEmptyDirectoryPlaceholders(new File(testDir))
   edu.zsd.scomm.mainWindow.main(Array(testDir))
 
   val robot: Robot = BasicRobot.robotWithCurrentAwtHierarchy
@@ -26,4 +27,17 @@ object FESTTest {
     val left = new DirectoryListUserActions("directoriesPane.left")
     val right = new DirectoryListUserActions("directoriesPane.right")
   }
+
+  def deleteEmptyDirectoryPlaceholders(dir: File) {
+    val emptyPlaceholder: File = new File(dir, "empty")
+    if (emptyPlaceholder.exists()) {
+      emptyPlaceholder.delete()
+    }
+    val files = dir.listFiles()
+    if (files != null) {
+      for (file <- files if file.isDirectory) deleteEmptyDirectoryPlaceholders(file)
+    }
+  }
+
+
 }
