c85e4abe20babd52c091255b3f253a34a7fb2acb
Zsolt Donca <dnczsolt@gmail.com>
2014-05-25 22:08:59 +0300

    Continuations Swing app refactored.

--- scomm/src/main/scala/edu/zsd/cpsdemo/ContinuationsSwingApp.scala
+++ scomm/src/main/scala/edu/zsd/cpsdemo/ContinuationsSwingApp.scala
@@ -2,47 +2,40 @@ package edu.zsd.cpsdemo
 
 import scala.util.continuations._
 import scala.swing._
-import scala.swing.BorderPanel.Position
-import scala.swing.event.MouseClicked
 
 object ContinuationsSwingApp extends SimpleSwingApplication {
 
-  override def top: swing.Frame = new MainFrame {
-    contents = new BorderPanel {
-      val button = new Button("Next")
-      add(button, Position.South)
+  var continue: (Unit => Unit) = { _ => run()}
 
-      val textField = new TextArea(10, 40)
-      add(textField, Position.Center)
-
-      val label = new Label("Welcome to the demo app")
-      add(label, Position.North)
-
-      listenTo(button.mouse.clicks)
-      reactions += {
-        case MouseClicked(_, _, _, _, _) => eventHandler()
-      }
-
-      var eventHandler: (Unit => Unit) = { _ => run()}
-
-      def run() {
-        reset {
-          val firstName = getResponse("What is your first name?")
-          val lastName = getResponse("What is your last name?")
-          label.text = s"Hello, $firstName $lastName!"
-        }
-      }
-
-      def getResponse(prompt: String): String@cpsParam[Unit, Unit] = {
-        label.text = prompt
-        shift {
-          k: (Unit => Unit) => {
-            eventHandler = k
-          }
-        }
-        textField.text
-      }
+  def run() {
+    reset {
+      val first = ask("What is your first name?")
+      val last = ask("What is your last name?")
+      label.text = s"Hello, $first $last!"
     }
   }
 
+  val textField = new TextArea(10, 40)
+  val label = new Label("Welcome to the demo app")
+  val button = new Button(new Action("Next") {
+    override def apply(): Unit = continue()
+  })
+
+  def ask(prompt: String): String@cpsParam[Unit, Unit] = {
+    label.text = prompt
+    shift {
+      k: (Unit => Unit) => {
+        continue = k
+      }
+    }
+    textField.text
+  }
+
+  override def top: swing.Frame = new MainFrame {
+    contents = new BorderPanel {
+      add(label, BorderPanel.Position.North)
+      add(textField, BorderPanel.Position.Center)
+      add(button, BorderPanel.Position.South)
+    }
+  }
 }
