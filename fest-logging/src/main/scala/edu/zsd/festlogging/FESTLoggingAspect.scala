5e0b7502f44d2da153cbd8c75418717b500743ea
Zsolt Donca <dnczsolt@gmail.com>
2014-06-11 18:32:30 +0300

    We don't need basic robot and we don't need to wave fest packages either!

--- fest-logging/src/main/scala/edu/zsd/festlogging/FESTLoggingAspect.scala
+++ fest-logging/src/main/scala/edu/zsd/festlogging/FESTLoggingAspect.scala
@@ -31,11 +31,8 @@ class FESTLoggingAspect {
   @Pointcut("execution(@edu.zsd.festlogging.ExecuteInEDT * *.*(..))")
   def executeInEDTMethods() {}
 
-  @Pointcut("execution(public * org.fest.swing.core.BasicRobot.*(..))")
-  def robotMethods() {}
-
   @Around("executeInEDTMethods()")
-  def executeInEDT(joinPoint: ProceedingJoinPoint) : AnyRef = {
+  def executeInEDT(joinPoint: ProceedingJoinPoint): AnyRef = {
     GuiActionRunner.execute(new GuiQuery[AnyRef] {
       override def executeInEDT(): AnyRef = joinPoint.proceed()
     })
@@ -56,12 +53,12 @@ class FESTLoggingAspect {
   }
 
   @Around("testMethods() || beforeMethods() || afterMethods()")
-  def takeScreenshotOfFailedTest(joinPoint: ProceedingJoinPoint) : AnyRef = {
+  def takeScreenshotOfFailedTest(joinPoint: ProceedingJoinPoint): AnyRef = {
 
     try {
       joinPoint.proceed()
     } catch {
-      case e : Throwable =>
+      case e: Throwable =>
         val testMethodExecution: RunningTestMethodExecution = MethodCallStack.getCurrentRunningTestMethodExecution(joinPoint)
         testMethodExecution.afterScreenshot = Some(takeScreenshot("failed"))
         throw e
@@ -69,7 +66,7 @@ class FESTLoggingAspect {
   }
 
   @Around("guiTestActionMethods()")
-  def takeScreenshotsOfTestActions(joinPoint: ProceedingJoinPoint) : AnyRef = {
+  def takeScreenshotsOfTestActions(joinPoint: ProceedingJoinPoint): AnyRef = {
     val testMethodExecution: RunningTestMethodExecution = MethodCallStack.getCurrentRunningTestMethodExecution(joinPoint)
     testMethodExecution.beforeScreenshot = Some(takeScreenshot("before"))
     try {
@@ -79,7 +76,7 @@ class FESTLoggingAspect {
     }
   }
 
-  private def takeScreenshot(kind : String) : File = {
+  private def takeScreenshot(kind: String): File = {
     val filename: String = f"$currentTestIndex%04d_${Platform.currentTime}_$kind.${ImageFileExtensions.PNG}"
     val beforeScreenshot: File = new File(screenshotsDir, filename)
     new ScreenshotTaker().saveDesktopAsPng(beforeScreenshot.getPath)
